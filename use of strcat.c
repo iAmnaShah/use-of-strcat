53606c8377d91330a916534dd6a92ac0f3ade7bb
iAmnaShah <120739494+iAmnaShah@users.noreply.github.com>
2023-02-11 20:07:19 +0500

    Add files via upload

---
+++ use of strcat.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char s1[15];
+    char s2[15];
+    printf("Enter 1st string:");
+    scanf("%s",s1);
+    printf("Enter 2nd string:");
+    scanf("%s",s2);
+    strcat(s1, s2);
+    printf("%s %s",s1, s2);
+    return 0;
+}
